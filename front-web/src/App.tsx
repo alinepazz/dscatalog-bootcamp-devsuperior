8329d4233fb055efc57ee0f1d77b8c1118b37d75
Aline Paz <aline.soares.2@hotmail.com>
2021-03-05 18:55:45 -0300

    front-web

---
+++ front-web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Alert from './Alert';
+
+const App = () => {
+    return (
+       <div className= "container mt-5">
+            <Alert text= "Aline" />
+            <Alert text= "Hello Brasil"/>
+            <Alert text="Hello bds2" />
+            <Alert />
+       </div>
+    );
+} 
+
+export default App;
